4a65c348518ade2969acb029b30fd2f8099826ae
Michel Silva <michelpablo7@gmail.com>
2026-08-05 16:31:49 -0300

    learn: variaveis

--- intro_javascript/script.js
+++ intro_javascript/script.js
@@ -1,3 +1,11 @@
 //começando...
 //const, let, var
 // a = 2 ou var = 2 ou a = abc
+
+//const nome = 'Jose'   //const não pode ser reatribuida
+//console.log(nome)    //printf do JS
+
+let nome = "Ana"    //let pode ser reatribuido
+console.log(nome)
+nome = "Ana Maria"
+console.log(nome)
