9787f05ae4a1d473a2dff9a515ca7c240ba4d504
Michel Silva <michelpablo7@gmail.com>
2026-08-05 17:32:29 -0300

    learn: dupla passagem

--- intro_javascript/script.js
+++ intro_javascript/script.js
@@ -5,7 +5,14 @@
 //const nome = 'Jose'   //const não pode ser reatribuida
 //console.log(nome)    //printf do JS
 
-let nome = "Ana"    //let pode ser reatribuido
+let nome = "Ana"    //let pode ser reatribuido, mas não pode ser redeclarado, mais usado
 console.log(nome)
 nome = "Ana Maria"
 console.log(nome)
+
+var nome2 = "Mario"    //var pode ser redeclarado, tem içamento
+console.log(nome2)
+var nome2 = "Mario Jr"
+console.log(nome2)
+
+    
